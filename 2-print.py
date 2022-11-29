96a07e2d3798117f8643800ac19e70cb27d41fe7
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-11-28 18:56:48 -0800

    adding Directory corrections

--- 2-print.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-# Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
-
-print('"Programming is like building a multilingual puzzle')
--- 4-print_float.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-number = 3.14159
-print("Float: {}\n".format(round(number, 2)))
