e36542665f1a97a56fa6739f4e13a7a162281727
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-11-28 19:21:09 -0800

    Adding 2-print.py correction

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
 
-print('"Programming is like building a multilingual puzzle')
+print(f"\"Programming is like building a multilingual puzzle")
