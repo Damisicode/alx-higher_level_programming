fd2b7f816b2ac8b4c6629a6f1e95d109c093f408
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-11-29 20:23:22 -0800

    Adding 5-print_comb2 correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i != 99:
-        print("%02d, " % (i), end="")
-    else:
-        print("99")
+print(", ".join("{0:0>2}".format(i) for i in range(100)))
