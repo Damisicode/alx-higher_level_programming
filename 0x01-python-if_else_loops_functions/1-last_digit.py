ed31268d508f8e313632c733fbacc45ceda7ae92
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2022-11-29 19:34:45 -0800

    Adding 1-last_digit.py solutions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,8 +11,9 @@ while last_number > 9:
     last_number %= 10
 last_number *= coeff
 if last_number < 6 and last_number != 0:
-    print(f"Last digit of {number} is {last_number} and is less than 6 and not 0\n")
+    print(f"Last digit of {number} is {last_number} and is less than 6 and
+ not 0")
 elif last_number > 5 and last_number != 0:
-    print(f"Last digit of {number} is {last_number} and is greater than 5\n")
+    print(f"Last digit of {number} is {last_number} and is greater than 5")
 else:
-    print(f"Last digit of {number} is {last_number} and is 0\n")
+    print(f"Last digit of {number} is {last_number} and is 0")
